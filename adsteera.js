da93b0fa6461c0212b0c5b1698ec5d1d9581cea1
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2022-12-30 14:43:43 +0600

    Create adsteera.js

---
+++ adsteera.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'c3a810c97681cbcd1c1ba12dad0b95d0',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/c3a810c97681cbcd1c1ba12dad0b95d0/invoke.js"></scr' + 'ipt>');
