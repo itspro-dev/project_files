110dcf51d3652c3b9ca03f37c6335a304eae7f6b
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2023-01-25 00:42:59 +0600

    Create ads2.js

---
+++ ads2.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '5c0f5edb417c6acd6c44a6f63a7de803',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/5c0f5edb417c6acd6c44a6f63a7de803/invoke.js"></scr' + 'ipt>');
+</script>
