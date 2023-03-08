b9bdd0c6283f6b8e6b81f1b622ad06db31383249
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2023-03-08 19:37:26 +0600

    Update adsteera.js

--- adsteera.js
+++ adsteera.js
@@ -1,8 +1,8 @@
-	atOptions = {
+atOptions = {
 		'key' : 'c3a810c97681cbcd1c1ba12dad0b95d0',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/c3a810c97681cbcd1c1ba12dad0b95d0/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/c3a810c97681cbcd1c1ba12dad0b95d0/invoke.js"></scr' + 'ipt>');
