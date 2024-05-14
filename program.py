df0e92a706ef8e253a0e51ce1d6e9b7644223729
Alok <84543009+itspro-dev@users.noreply.github.com>
2024-05-15 00:26:47 +0600

    Update program.py

--- program.py
+++ program.py
@@ -54,5 +54,20 @@ for i in 'complete':
 for i in 'complete' :
     if i == 'l':
         continue
-    print(i)
+    print(I)
+
+# code to find factorial of a single number
+
+number = int(input("Enter factorial number: "))
+factorial = 1
+
+if number < 0:
+    print("Sorry, the factorial number doesn't exist for negative numbers.")
+elif number == 0:
+    print("The factorial of 0 is: 1")
+else:
+    for i in range(1, number + 1):
+        factorial *= i
+    print("The factorial of", number, "is:", factorial)
+
 
